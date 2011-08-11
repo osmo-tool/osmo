20aed9d8154ad6c3315c2259d499d22097cfa90a
OPPOLLI <OPPOLLI@OULKV10177.ad.vtt.fi>
2011-08-11 16:52:05 +0300

    Requirement coverage counting added and refactoring

--- osmotester/src/osmo/tester/coverage/HTML.java
+++ osmotester/src/osmo/tester/coverage/HTML.java
@@ -17,13 +17,13 @@ public class HTML extends CoverageMetric{
   }
 
   @Override
-  public String getTransitionCoverage() {
+  public String getTransitionCounts() {
     String ret = "<html>\n";
     ret += "<head></head>\n";
     ret += "<body>\n";
     ret += "<table border=\"1\">\n";
     ret += "<tr><td>Name</td><td>Count</td></tr>\n";
-    Map<FSMTransition, Integer> coverage = countTransitionCoverage();
+    Map<FSMTransition, Integer> coverage = countTransitions();
      for(Map.Entry<FSMTransition, Integer> a : coverage.entrySet()){
        ret += "<tr><td>"+ a.getKey().getName()+"</td><td>"+a.getValue()+"</td></tr>\n";
      }
@@ -34,13 +34,13 @@ public class HTML extends CoverageMetric{
   }
 
   @Override
-  public String getTransitionPairCoverage(){
+  public String getTransitionPairCounts(){
     String ret = "<html>\n";
     ret += "<head></head>\n";
     ret += "<body>\n";
     ret += "<table border=\"1\">\n";
     ret += "<tr><td>From</td><td>To</td><td>Count</td></tr>\n";
-    Map<String, Integer> coverage = countTransitionPairCoverage();
+    Map<String, Integer> coverage = countTransitionPairs();
     for(Map.Entry<String, Integer> a : coverage.entrySet()){
       String[] b = a.getKey().split(";");
       ret += "<tr><td>"+ b[0]+"</td><td>"+ b[1]+"</td><td>"+a.getValue()+"</td></tr>\n";
@@ -52,15 +52,26 @@ public class HTML extends CoverageMetric{
   }
 
   @Override
-  public String getRequirementsCoverage() {
-    // TODO Auto-generated method stub
-    return null;
+  public String getRequirementsCounts() {
+    String ret = "<html>\n";
+    ret += "<head></head>\n";
+    ret += "<body>\n";
+    ret += "<table border=\"1\">\n";
+    ret += "<tr><td>Name</td><td>Count</td></tr>\n";
+    Map<String, Integer> coverage = countRequirements();
+    for(Map.Entry<String, Integer> a : coverage.entrySet()){
+      ret += "<tr><td>"+a.getKey()+"</td><td>"+a.getValue()+"</td></tr>\n";
+    }
+    ret += "</table>\n";
+    ret += "</body>\n";
+    ret += "</html>\n";
+    return ret;
   }
 
   @Override
   public String getTraceabilityMatrix() {
     // TODO Auto-generated method stub
-    return null;
+    return "Not implemented yet";
   }
 
 }
