a3513ab01667df0400f628805261dee99f8b2ae7
tkanstren <tkanstren@gmail.com>
2011-08-21 23:34:52 +0300

    some gui drafting

--- miner/src/osmo/miner/Main.java
+++ miner/src/osmo/miner/Main.java
@@ -1,7 +1,39 @@
 package osmo.miner;
 
+import javax.swing.*;
+import javax.swing.plaf.TreeUI;
+import javax.swing.plaf.basic.BasicTreeUI;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import java.awt.*;
+import java.io.InputStream;
+import java.net.URL;
+
 /**
  * @author Teemu Kanstren
  */
 public class Main {
+  public static void main(String[] args) throws Exception {
+    new Main().main2();
+  }
+
+  public void main2() throws Exception {
+    JFrame frame = new JFrame("OSMO Miner");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    try {
+      for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+        if ("Nimbus".equals(info.getName())) {
+          UIManager.setLookAndFeel(info.getClassName());
+          break;
+        }
+      }
+    } catch (Exception e) {
+      //if Nimbus does not exist, you are out of luck
+      System.out.println("No Nimbus look and feel found. The GUI is not going to be pretty now..");
+    }
+    JTree files = new JTree();
+
+    frame.getContentPane().add(files, BorderLayout.WEST);
+    frame.setSize(800, 600);
+    frame.setVisible(true);
+  }
 }
