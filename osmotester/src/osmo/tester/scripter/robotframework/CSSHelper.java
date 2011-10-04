e424a41ce98be7bf67a1d1758cb0387725243c2a
tkanstren <tkanstren@gmail.com>
2011-10-04 13:08:16 +0300

    [tester] [updated missing javadocs] [updated manual to complete the calendar tutorial]

--- osmotester/src/osmo/tester/scripter/robotframework/CSSHelper.java
+++ osmotester/src/osmo/tester/scripter/robotframework/CSSHelper.java
@@ -1,6 +1,8 @@
 package osmo.tester.scripter.robotframework;
 
 /**
+ * A helper class to pretty-format the velocity templates creating the robot framework test scripts.
+ *
  * @author Teemu Kanstren
  */
 public class CSSHelper {
