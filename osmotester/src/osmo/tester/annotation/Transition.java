ee648ee52740b0dc25fda096dbcf65cf5ca15d4a
tkanstren <tkanstren@gmail.com>
2011-10-28 15:16:44 +0300

    [tester] minor fixes to comments moved calendar main method to separate class

--- osmotester/src/osmo/tester/annotation/Transition.java
+++ osmotester/src/osmo/tester/annotation/Transition.java
@@ -31,7 +31,7 @@ import java.lang.annotation.Target;
  * taken when several are available. Weighted algorithms ({@link osmo.tester.generator.algorithm.WeightedRandomAlgorithm})
  * take available transitions based on their weight. The more weight the transition has, the more often it is taken.
  * If transitions "A" is given weight 1 and transitions B "2", then B is taken twice as often as A. A transition
- * without a weight defined has a default weight of 1.
+ * without a weight defined has a default weight of 10.
  *
  * @see Guard
  *
