49919a9a8d14578401ff397627bc6e8f17a1137a
tkanstren <tkanstren@gmail.com>
2012-01-29 12:43:58 +0200

    [tester] added javadocs for new code test improvements minor api cleanup tutorial final touches release 2.2.0

--- osmotester/src/osmo/tester/junit/JUnitGenerationListener.java
+++ osmotester/src/osmo/tester/junit/JUnitGenerationListener.java
@@ -7,10 +7,20 @@ import osmo.tester.generator.testsuite.TestSuite;
 import osmo.tester.model.FSM;
 import osmo.tester.model.FSMTransition;
 
-/** @author Teemu Kanstren */
+/**
+ * Used internally by OSMO Tester to make sure suite ending is handled properly.
+ * This mainly means ensuring {@link osmo.tester.annotation.AfterSuite} methods and similar
+ * teardown functionality is properly invoked.
+ * Needs a separate listener since JUnit does not seem to support this type of functionality nicely.
+ *
+ * @author Teemu Kanstren
+ */
 public class JUnitGenerationListener implements GenerationListener {
+  /** The expected number of tests to be generated. */
   private final int expected;
+  /** The actual number of tests generated so far. */
   private int actual = 0;
+  /** The generator used for generating the tests, and for running the teardown code when done. */
   private final MainGenerator generator;
 
   public JUnitGenerationListener(int expected, MainGenerator generator) {
@@ -42,6 +52,11 @@ public class JUnitGenerationListener implements GenerationListener {
   public void testStarted(TestCase test) {
   }
 
+  /**
+   * Here is whre we keep track of the number of generated tests.
+   *
+   * @param test The associated test object.
+   */
   @Override
   public void testEnded(TestCase test) {
     actual++;
