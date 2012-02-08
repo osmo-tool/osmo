e81cd46cb602f9802e943b1dde118a151305aebc
tkanstren <tkanstren@gmail.com>
2012-02-08 10:26:16 +0200

    [tester] fix for "n must be positive" error

--- osmotester/test/osmo/tester/algorithm/BalancingTests.java
+++ osmotester/test/osmo/tester/algorithm/BalancingTests.java
@@ -2,19 +2,21 @@ package osmo.tester.algorithm;
 
 import org.junit.Before;
 import org.junit.Test;
+import osmo.common.log.Logger;
 import osmo.tester.OSMOTester;
 import osmo.tester.generation.TestListener;
 import osmo.tester.generator.algorithm.BalancingAlgorithm;
 import osmo.tester.generator.algorithm.RandomAlgorithm;
 import osmo.tester.generator.endcondition.Length;
 import osmo.tester.testmodels.ValidTestModel6;
+import osmo.tester.testmodels.ValidTestModel7;
 
 import java.util.Collection;
 
 import static junit.framework.Assert.assertFalse;
 
 /** @author Teemu Kanstren */
-public class LessRandomTests {
+public class BalancingTests {
   private OSMOTester osmo = null;
   private TestListener listener;
 
@@ -53,9 +55,9 @@ public class LessRandomTests {
   public void testModel6Length20() {
     listener.addExpected("suite-start", "start", "t:t1", "t:t1", "t:t2", "t:t1", "t:t4", "t:t3", "t:t4", "t:t4", "t:t1", "t:t3", "t:t2", "t:t4", "t:t2", "t:t3", "t:t3", "t:t1", "t:t1", "t:t3", "t:t1", "t:t2", "end", "suite-end");
     osmo.addModelObject(new ValidTestModel6());
-    Length length15 = new Length(20);
+    Length length20 = new Length(20);
     Length length1 = new Length(1);
-    osmo.addTestEndCondition(length15);
+    osmo.addTestEndCondition(length20);
     osmo.addSuiteEndCondition(length1);
     osmo.setAlgorithm(new BalancingAlgorithm());
     osmo.generate();
@@ -64,11 +66,34 @@ public class LessRandomTests {
 
     testSetup();
     osmo.addModelObject(new ValidTestModel6());
-    osmo.addTestEndCondition(length15);
+    osmo.addTestEndCondition(length20);
     osmo.addSuiteEndCondition(length1);
     osmo.setAlgorithm(new RandomAlgorithm());
     osmo.generate();
     Collection<String> random = listener.getSteps();
     assertFalse("Optimized generator should be different from random", random.equals(optimized));
+  }
+
+  @Test
+  public void testModel6Length10000() {
+    osmo.addModelObject(new ValidTestModel6());
+    Length length1000 = new Length(1000);
+    Length length10 = new Length(10);
+    osmo.addTestEndCondition(length1000);
+    osmo.addSuiteEndCondition(length10);
+    osmo.setAlgorithm(new BalancingAlgorithm());
+    osmo.generate();
+  }
+
+  @Test
+  public void testModel7Length200() {
+//    Logger.debug = true;
+    osmo.addModelObject(new ValidTestModel7());
+    Length length200= new Length(200);
+    Length length1 = new Length(1);
+    osmo.addTestEndCondition(length200);
+    osmo.addSuiteEndCondition(length1);
+    osmo.setAlgorithm(new BalancingAlgorithm());
+    osmo.generate();
   }
 }
---
+++ osmotester/test/osmo/tester/testmodels/ValidTestModel7.java
@@ -0,0 +1,74 @@
+package osmo.tester.testmodels;
+
+import osmo.tester.annotation.Guard;
+import osmo.tester.annotation.TestStep;
+import osmo.tester.annotation.Transition;
+
+import java.io.PrintStream;
+
+/** @author Teemu Kanstren */
+public class ValidTestModel7 {
+  private int count1 = 0;
+  private int count2 = 0;
+  private int count3 = 0;
+  private int count4 = 0;
+  private final PrintStream out;
+
+  //TODO: remove this constructor and add checks
+  public ValidTestModel7() {
+    out = null;
+  }
+
+  public ValidTestModel7(PrintStream out) {
+    this.out = out;
+  }
+
+  //todo: give means for guard to access information on which step is being guarded
+  @Guard("t1")
+  public boolean allowOne() {
+    if (count1 < 10) {
+      return true;
+    }
+    return count2 > 20;
+  }
+
+  @Guard("t4")
+  public boolean allowFour() {
+    if (count4 < 10) {
+      return true;
+    }
+    return count2 > 20;
+  }
+
+  @Guard("t3")
+  public boolean allowThree() {
+    if (count3 < 10) {
+      return true;
+    }
+    return count2 > 20;
+  }
+
+  @TestStep("t1")
+  public void one() {
+    System.out.println("T1");
+    count1++;
+  }
+
+  @TestStep("t2")
+  public void two() {
+    System.out.println("T2");
+    count2++;
+  }
+
+  @TestStep("t3")
+  public void three() {
+    System.out.println("T3");
+    count3++;
+  }
+
+  @TestStep("t4")
+  public void four() {
+    System.out.println("T4");
+    count4++;
+  }
+}
