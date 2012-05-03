86f6b23cbac2d8b1248b9ef004060c862c6d90d4
tkanstren <tkanstren@gmail.com>
2012-05-03 13:16:56 +0300

    jenkins example drafting

---
+++ examples/src/osmo/tester/examples/report/CalendarJenkins.java
@@ -0,0 +1,34 @@
+package osmo.tester.examples.report;
+
+import osmo.tester.OSMOConfiguration;
+import osmo.tester.OSMOTester;
+import osmo.tester.examples.calendar.scripter.CalendarScripter;
+import osmo.tester.examples.calendar.scripter.offline.OfflineScripter;
+import osmo.tester.examples.calendar.testmodel.*;
+import osmo.tester.generator.endcondition.Length;
+import osmo.tester.reporting.jenkins.JenkinsReportGenerator;
+
+/** @author Teemu Kanstren */
+public class CalendarJenkins {
+  public static void main(String[] args) throws Exception {
+    OSMOTester osmo = new OSMOTester();
+    OSMOConfiguration config = new OSMOConfiguration();
+    JenkinsReportGenerator reporter = new JenkinsReportGenerator();
+    config.addListener(reporter);
+    osmo.setConfig(config);
+    osmo.addSuiteEndCondition(new Length(20));
+    ModelState state = new ModelState();
+    CalendarScripter scripter = new OfflineScripter("tests.html");
+    osmo.addModelObject(state);
+    osmo.addModelObject(new CalendarBaseModel(state, scripter));
+    osmo.addModelObject(new CalendarOracleModel(state, scripter));
+    osmo.addModelObject(new CalendarTaskModel(state, scripter));
+    osmo.addModelObject(new CalendarOverlappingModel(state, scripter));
+    osmo.addModelObject(new CalendarParticipantModel(state, scripter));
+    osmo.addModelObject(new CalendarErrorHandlingModel(state, scripter));
+    osmo.addModelObject(new CalendarFailureModel(state, scripter));
+    osmo.generate();
+    reporter.writeStepReport("calendar-steps.xml");
+    reporter.writeTestReport("calendar-tests.xml");
+  }
+}
