f93895bef3be04edec1472383746620a56f487dd
tkanstren <tkanstren@localhost>
2012-10-12 14:19:20 -0400

    base scripter first draft

---
+++ osmotester/src/osmo/tester/scripter/Scripter.java
@@ -0,0 +1,15 @@
+package osmo.tester.scripter;
+
+/** 
+ * Defines a general scripting interface.
+ * Currently a first draft for future extension.
+ * 
+ * @author Teemu Kanstren 
+ */
+public interface Scripter {
+  public void startSuite();
+  public void startTest();
+  public void step(String name, Object... params);
+  public void endTest();
+  public void endSuite();
+}
