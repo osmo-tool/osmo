ce47b147320ddce7005c747a2d4429f93bad57cb
tkanstren <tkanstren@localhost>
2013-03-24 22:15:44 -0400

    deleted miner

--- miner/src/osmo/miner/testminer/dataflow/ValueInCollectionMiner.java
+++
@@ -1,9 +0,0 @@
-package osmo.miner.testminer.dataflow;
-
-/**
- * Is the value always in a collection?
- *
- * @author Teemu Kanstren
- */
-public class ValueInCollectionMiner {
-}
--- miner/test/osmo/miner/parser/xml/XMLParserTests.java
+++
@@ -1,9 +0,0 @@
-package osmo.miner.parser.xml;
-
-import org.junit.Test;
-
-public class XMLParserTests {
-  @Test
-  public void parseXML() {
-  }
-}
