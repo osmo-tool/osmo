e7cfc60d30f45b8745a10f71a9ba82d5d4960dd1
tkanstren <tkanstren@localhost>
2013-07-31 13:38:46 +0300

    added "visualizer" to print the given model structure.

--- osmotester/src/osmo/tester/parser/annotation/BeforeSuiteParser.java
+++ osmotester/src/osmo/tester/parser/annotation/BeforeSuiteParser.java
@@ -1,11 +1,14 @@
 package osmo.tester.parser.annotation;
 
 import osmo.common.log.Logger;
+import osmo.tester.annotation.AfterTest;
 import osmo.tester.annotation.BeforeSuite;
 import osmo.tester.model.InvocationTarget;
 import osmo.tester.parser.AnnotationParser;
 import osmo.tester.parser.ParserParameters;
 import osmo.tester.parser.ParserResult;
+
+import java.lang.reflect.Method;
 
 /**
  * Parses {@link osmo.tester.annotation.BeforeSuite} annotations from the given model object.
@@ -19,6 +22,12 @@ public class BeforeSuiteParser implements AnnotationParser {
   public String parse(ParserResult result, ParserParameters parameters) {
     BeforeSuite before = (BeforeSuite) parameters.getAnnotation();
     result.getFsm().addBeforeSuite(new InvocationTarget(parameters, BeforeSuite.class));
-    return "";
+    Method method = parameters.getMethod();
+    String errors = "";
+    Class<?>[] parameterTypes = method.getParameterTypes();
+    if (parameterTypes.length > 0) {
+      errors += "@" + BeforeSuite.class.getSimpleName() + " methods are not allowed to have parameters: \"" + method.getName() + "()\" has " + parameterTypes.length + " parameters.\n";
+    }
+    return errors;
   }
 }
--- osmotester/src/osmo/tester/parser/annotation/BeforeTestParser.java
+++ osmotester/src/osmo/tester/parser/annotation/BeforeTestParser.java
@@ -1,11 +1,14 @@
 package osmo.tester.parser.annotation;
 
 import osmo.common.log.Logger;
+import osmo.tester.annotation.BeforeSuite;
 import osmo.tester.annotation.BeforeTest;
 import osmo.tester.model.InvocationTarget;
 import osmo.tester.parser.AnnotationParser;
 import osmo.tester.parser.ParserParameters;
 import osmo.tester.parser.ParserResult;
+
+import java.lang.reflect.Method;
 
 /**
  * Parses {@link osmo.tester.annotation.BeforeTest} annotations from the given model object.
@@ -19,6 +22,12 @@ public class BeforeTestParser implements AnnotationParser {
   public String parse(ParserResult result, ParserParameters parameters) {
     BeforeTest before = (BeforeTest) parameters.getAnnotation();
     result.getFsm().addBefore(new InvocationTarget(parameters, BeforeTest.class));
-    return "";
+    Method method = parameters.getMethod();
+    String errors = "";
+    Class<?>[] parameterTypes = method.getParameterTypes();
+    if (parameterTypes.length > 0) {
+      errors += "@" + BeforeTest.class.getSimpleName() + " methods are not allowed to have parameters: \"" + method.getName() + "()\" has " + parameterTypes.length + " parameters.\n";
+    }
+    return errors;
   }
 }
