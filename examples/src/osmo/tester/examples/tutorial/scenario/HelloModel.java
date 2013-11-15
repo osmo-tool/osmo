8023f76bb60d92beda983dfae06ee6886be150fa
tkanstren <tkanstren@localhost>
2013-11-15 14:34:46 -0500

    documentation updates + scenario tutorial example

--- examples/src/osmo/tester/examples/tutorial/scenario/HelloModel.java
+++ examples/src/osmo/tester/examples/tutorial/scenario/HelloModel.java
@@ -1,4 +1,4 @@
-package osmo.tester.examples.tutorial.data;
+package osmo.tester.examples.tutorial.scenario;
 
 import osmo.tester.annotation.AfterTest;
 import osmo.tester.annotation.BeforeSuite;
@@ -9,21 +9,12 @@ import osmo.tester.model.data.DataGenerationStrategy;
 import osmo.tester.model.data.ValueSet;
 
 /** @author Teemu Kanstren */
-public class HelloModel2 {
-  private int helloCount = 0;
-  private int worldCount = 0;
+public class HelloModel {
   private ValueSet<String> names = new ValueSet<>("teemu", "bob");
   private ValueSet<String> worlds = new ValueSet<>("mars", "venus");
 
-  @BeforeSuite
-  public void init() {
-    names.setStrategy(DataGenerationStrategy.BALANCING);
-  }
-
   @BeforeTest
   public void startTest() {
-    helloCount = 0;
-    worldCount = 0;
     System.out.println("TEST START");
   }
 
@@ -32,25 +23,13 @@ public class HelloModel2 {
     System.out.println("TEST END");
   }
 
-  @Guard("hello")
-  public boolean thisNameReallyIsIrrelevant() {
-    return helloCount == worldCount;
-  }
-
   @TestStep("hello")
   public void sayHello() {
     System.out.println("HELLO "+names.next());
-    helloCount++;
-  }
-
-  @Guard("world")
-  public boolean thisNameIsIrrelevant() {
-    return helloCount > worldCount;
   }
 
   @TestStep("world")
   public void sayWorld() {
     System.out.println("WORLD "+worlds.next());
-    worldCount++;
   }
 }
