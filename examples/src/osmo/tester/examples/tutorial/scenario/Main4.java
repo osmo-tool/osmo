fbe034b68ef4a17e8cb8f42b6730412c435f12c2
tkanstren <tkanstren@localhost>
2013-11-15 14:34:46 -0500

    documentation updates + scenario tutorial example

---
+++ examples/src/osmo/tester/examples/tutorial/scenario/Main4.java
@@ -0,0 +1,19 @@
+package osmo.tester.examples.tutorial.scenario;
+
+import osmo.tester.OSMOTester;
+import osmo.tester.generator.endcondition.Length;
+import osmo.tester.scenario.Scenario;
+
+/** @author Teemu Kanstren */
+public class Main4 {
+  public static void main(String[] args) {
+    OSMOTester osmo = new OSMOTester();
+    osmo.addModelObject(new HelloModel());
+    Scenario scenario = new Scenario(false);
+    scenario.forbid("world");
+    osmo.getConfig().setScenario(scenario);
+    osmo.setTestEndCondition(new Length(5));
+    osmo.setSuiteEndCondition(new Length(2));
+    osmo.generate(55);
+  }
+}
