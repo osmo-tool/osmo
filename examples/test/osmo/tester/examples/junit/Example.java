f75790e80bae3709fe4a78479489b2eb8eba2aaf
tkanstren <tkanstren@gmail.com>
2016-10-14 11:49:32 +0300

    moved junit integration code to examples to avoid cluttering projects with excess junit imports when using osmotester with other frameworks/versions

---
+++ examples/test/osmo/tester/examples/junit/Example.java
@@ -0,0 +1,39 @@
+package osmo.tester.examples.junit;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import osmo.tester.OSMOConfiguration;
+import osmo.tester.examples.calculator.CalculatorModel;
+import osmo.tester.generator.SingleInstanceModelFactory;
+import osmo.tester.reporting.junit.OSMOConfigurationProvider;
+import osmo.tester.reporting.junit.OSMORunner;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/** @author Teemu Kanstren */
+@RunWith(OSMORunner.class)
+public class Example {
+  private static ByteArrayOutputStream out;
+  private static PrintStream ps;
+
+  static {
+    out = new ByteArrayOutputStream(1000);
+    ps = new PrintStream(out);
+  }
+
+  @Before
+  public void hi() {
+    ps.println("hi there");
+  }
+
+  @OSMOConfigurationProvider
+  public static OSMOConfiguration giefConf() {
+    OSMOConfiguration config = new OSMOConfiguration();
+    SingleInstanceModelFactory factory = new SingleInstanceModelFactory();
+    factory.add(new CalculatorModel(ps));
+    config.setFactory(factory);
+    config.setJUnitLength(5);
+    return config;
+  }
+}
